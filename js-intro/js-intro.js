6284cdf9eaf66dd97ef507b20a7f065d2c7629c1
Ayoralol <121880251+Ayoralol@users.noreply.github.com>
2023-12-04 13:15:49 +1100

    js intro stuff

--- js-intro/js-intro.js
+++ js-intro/js-intro.js
@@ -33,3 +33,32 @@ console.log(
     age +
     " years old"
 );
+
+console.log(
+  2 == "2", // true
+  2 === 2, // true
+  2 === "2", // false
+  "2" + 3 == 23, // true
+  "2" + 3 === 5, // false
+  10 % 3, // 1
+  10 % 3 === 1, // true
+  (100 % 7) % 5, // 2
+  (100 % 7) % 5 !== 0, // true
+  (100 % 7) % 9 == 2, // true
+  !(10 % 2), // true
+  !!!!((10 % 7) % 3), // false
+  10 % 3 === -1 % 2, // false
+  // Continue here tomorrow ---
+  (892783 != "89278" + 3) == 0, // false ----- true - realise the answer
+  true && false, // false
+  false || true, // true
+  true || false, // true
+  true || false, // true
+  true || false + true, // true
+  true * false && false + true, // NaN ----- 0 - realise the answer
+  10 && 123 && -1 && 3, // 3
+  10 && 123 && 0 && 3, // 0
+  (10 && (123 || "") && parseInt("Roisin")) || 23 / 23 - 1, // -1 ------ 0 - realise the answer, math error by me
+  3 && "Calum" && ("" || 26), // 26
+  3 && "Remi" && (null || 86 * 0 || ("" + 1 && "Cat" + "Dog")) // "CatDog"
+);
