c8748a1981c6fa3d412669e1aec976447e2bbe38
Ayoralol <121880251+Ayoralol@users.noreply.github.com>
2023-12-07 13:43:09 +1100

    js loops iterations

--- js/loops/loops.js
+++ js/loops/loops.js
@@ -11,3 +11,31 @@ console.log(sum(10));
 let n = 10;
 
 console.log((n * (n + 1)) / 2);
+
+// another loops
+
+const pets = ["pixie", "claude", "sebastian", "bug"];
+// -> 1. ['claude', 'sebastian', 'bug', 'pixie']
+pets.push(pets.shift());
+console.log(pets);
+
+// -> 2. ['pixie', 'bug', 'sebastian', 'claude']
+pets.reverse();
+console.log(pets);
+
+// -> 3. ['bug', 'sebastian', 'pixie']
+pets.pop();
+pets.push(pets.shift());
+console.log(pets);
+// -> 4. ['Bug', 'Sebastian', 'Pixie]
+
+for (let i = 0; i < pets.length; i++) {
+  pets[i] = pets[i][0].toUpperCase() + pets[i].slice(1);
+}
+console.log(pets);
+// -> 5. ['Bug', 'Seb', 'Pix']
+
+for (let i = 0; i < pets.length; i++) {
+  pets[i] = pets[i].substring(0, 3);
+}
+console.log(pets);
